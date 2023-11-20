3d865b51f68e9fd55b079e7625fa0bbd4eab0b2f
sairakausar <sairakausar3@gmail.com>
2023-11-19 22:33:05 -0800

    Next.js_Assignmentsnov23

---
+++ Next.js/app/page.tsx
@@ -0,0 +1,12 @@
+import Image from 'next/image'
+
+export default function Home() {
+  return (
+    <main>
+      <div>
+        <h1>Saira Kausar jlcsdjdfl</h1>
+        <p>hdlshdfl</p>
+      </div>
+    </main>
+  )
+}
