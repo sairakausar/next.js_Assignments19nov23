e2e96f1cc525fc2a9919078aa753710c8894cd0e
sairakausar <sairakausar3@gmail.com>
2023-12-02 03:56:49 -0800

    update

--- Assignments_Next.js/assignment3_repeate_feching_and_dynamic_routing/lib/getUserPosts.tsx
+++ Assignments_Next.js/assignment3_repeate_feching_and_dynamic_routing/lib/getUserPosts.tsx
@@ -1,6 +1,9 @@
 
 
 export default async function getUserPosts() {
-   const userPost =  await fetch("https://jsonplaceholder.typicode.com/posts")
+    const userPost = await fetch(`https://jsonplaceholder.typicode.com/posts?id=${}`)
+    
+    // how ew know how we put https://jsonplaceholder.typicode.com/posts?id=  it mean
+    //                                                          mean =  ?id=
   return userPost.json( )
 }
