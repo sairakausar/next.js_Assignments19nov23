42c00c39a71841d2233c08a29e06d1561bd39a93
sairakausar <sairakausar3@gmail.com>
2023-12-01 19:15:44 -0800

    update

--- Assignments_Next.js/assignment3_repeate_feching_and_dynamic_routing/app/users/[id]/page.tsx
+++ Assignments_Next.js/assignment3_repeate_feching_and_dynamic_routing/app/users/[id]/page.tsx
@@ -1,21 +1,20 @@
 import Link from "next/link"
 import getUser from "@/lib/getUser"
 
-interface Params {
-  params: {
-    id: string
-  }
-}                                    
-export default async function page({ params }: Params) {
-  const getData = await getUser(params.id)
+// interface Params {
+//   params: {
+//     id: string
+//   }
+// }                                    
+export default async function page({ params }: { params: { id: string } }) {
+  const getData = await getUser(params.id);
   return (
     <div style={{ color: "Green" }}>
       <b>{getData.name}</b>
 
-      <br />
+      
 
       <Link href="/users">Back to User</Link>
     </div>
   );
-
 }
--- Assignments_Next.js/assignment3_repeate_feching_and_dynamic_routing/lib/getUser.tsx
+++ Assignments_Next.js/assignment3_repeate_feching_and_dynamic_routing/lib/getUser.tsx
@@ -3,6 +3,6 @@
 export default async function getUser(id: string) {
   const result = await fetch(`https://jsonplaceholder.typicode.com/users/${id}`
             
-  );
+  )
   return result.json();
 }
