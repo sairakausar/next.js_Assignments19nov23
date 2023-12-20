60992c89a1b023c0165456390e7b530bd14e3ecc
sairakausar <sairakausar3@gmail.com>
2023-12-20 15:31:53 -0800

    update only folder for making form

---
+++ Assignments_Next.js/assignment5_make_form_with_yup/app/page.tsx
@@ -0,0 +1,67 @@
+import Image from 'next/image'
+
+export default function Home() {
+  return (
+    <main>
+      <form onSubmit={handleSubmit} className="max-w-md mx-auto">
+        <div className="mb-4">
+          <label
+            htmlFor="name"
+            className="block text-gray-700 text-sm font-bold mb-2"
+          >
+            Name
+          </label>
+          <input
+            type="text"
+            id="name"
+            name="name"
+            className="w-full px-3 py-2 border rounded-md"
+            required
+          />
+        </div>
+
+        <div className="mb-4">
+          <label
+            htmlFor="email"
+            className="block text-gray-700 text-sm font-bold mb-2"
+          >
+            Email
+          </label>
+          <input
+            type="email"
+            id="email"
+            name="email"
+            className="w-full px-3 py-2 border rounded-md"
+            required
+          />
+        </div>
+
+        <div className="mb-4">
+          <label
+            htmlFor="message"
+            className="block text-gray-700 text-sm font-bold mb-2"
+          >
+            Message
+          </label>
+          <textarea
+            id="message"
+            name="message"
+            rows={4}
+            className="w-full px-3 py-2 border rounded-md"
+            required
+          ></textarea>
+        </div>
+
+        <div className="mb-6">
+          <button
+            type="submit"
+            className="bg-blue-500 text-white py-2 px-4 rounded-md hover:bg-blue-600"
+          >
+            Submit
+          </button>
+        </div>
+      </form>
+
+    </main>
+  );
+}
