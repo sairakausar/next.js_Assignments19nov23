ce530fb85c61b10b51196c2a44f9edb84cc6d286
sairakausar <sairakausar3@gmail.com>
2023-12-21 20:05:24 -0800

    form functionality with 1 useSate Spred opertor with any type update

--- Assignments_Next.js/assignment5_yup_form_create_10entries/app/component/displaycontact.tsx
+++ Assignments_Next.js/assignment5_yup_form_create_10entries/app/component/displaycontact.tsx
@@ -1,11 +1,17 @@
 import React from 'react'
 
-export default function Displaycontact() {
+export default function Displaycontact(props: any) {
   return (
     <div className="flex items-center justify-center">
       <h1 className="text-3xl font-bold text-rose-600">
         {" "}
         Display Contact Child
+        <br />
+        name: {props.objectforChild.name}
+        <br />
+              email: {props.objectforChild.email}
+              <br />
+              message: {props.objectforChild.message}
       </h1>
     </div>
   );
--- Assignments_Next.js/assignment5_yup_form_create_10entries/app/component/formDocumentation.tsx
+++ Assignments_Next.js/assignment5_yup_form_create_10entries/app/component/formDocumentation.tsx
@@ -5,21 +5,47 @@ import Displaycontact from './displaycontact';
 
 export default function FormDocumentation() {
 
-  const [allTheContentImake1UseState, setallTheContentImake1UseState] =
+  const [allTheContactImake1UseState, setallTheContactImake1UseState] =
     useState({
-      name    :  "",
-      email   :  "",
-      message: 0
-      // we never use = sign in the object. In the object always use: 
+      name: "",
+      email: "",
+      message: "",
+      // we never use = sign in the object. In the object always use:
+      // 1) make the use state hook after makin the displaycontact.tsx
+      // allTheContactImake1UseState this is also object
     });
 
+  // 2) function make and this function put on all the value of name ,
+  // email, message and 2nd of his purpose is use Sate function use in it and 
+  // it set the the of 1st variable of useState . 3rd work in it UseSate 
+  //First variable of useSate using Spred opertor
+  // this open value and this opened value merge with the new value.
+  const allContactInfoHandler = (event: any ) => {  // event is complsory inspect
+    let allTheContactImake1UseStateMergerwithNewEntries = {
+      // this is obect allTheContactImake1UseStateMergerwithNewEntries
+      // 3) make object
+      ...allTheContactImake1UseState, // this value is open with spred
+      [event.target.name]: [event.target.value],
+    };
+      //3)
 
+      // :  []more value
+      // value assign with:
+      // it make run tine variable himsely by react for this purpose we put
+      // [] it make dynamic variable by himselfly and also chenged by himselfly
+      // 4)  [event.target.name] for changinging name value i time then chage
+      // the name to email and other with Dynamicly
+setallTheContactImake1UseState(allTheContactImake1UseStateMergerwithNewEntries);
+    //5) now his copy can come out with the st() allTheContactImake1UseStateMergerwithNewEntries
+    // 
+   }
 
     
   return (
     <div>
-      <Displaycontact />
-
+      {/* // 9) give to value of child for display front last this work but no 
+      display on front*/}
+      <Displaycontact objectforChild={allTheContactImake1UseState} />
 
       <form className="max-w-md mx-auto">
         <div className="mb-4">
@@ -30,6 +56,8 @@ export default function FormDocumentation() {
             Name
           </label>
           <input
+            //6) //2) wala which change value set the value
+            onChange={allContactInfoHandler}
             type="text"
             id="name"
             name="name"
@@ -46,6 +74,8 @@ export default function FormDocumentation() {
             Email
           </label>
           <input
+            //7) same give this funtion
+            onChange={allContactInfoHandler}
             type="email"
             id="email"
             name="email"
@@ -62,6 +92,8 @@ export default function FormDocumentation() {
             Message
           </label>
           <textarea
+            //8) text are = input
+            onChange={allContactInfoHandler}
             id="message"
             name="message"
             rows={4}
