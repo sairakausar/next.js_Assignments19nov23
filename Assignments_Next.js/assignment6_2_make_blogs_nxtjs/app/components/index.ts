5e0e1038d9812ee130c837f0c442a4311c3641b5
sairakausar <sairakausar3@gmail.com>
2023-12-27 09:14:40 -0800

    updateeeed

--- Assignments_Next.js/assignment6_2_make_blogs_nxtjs/app/components/index.ts
+++ Assignments_Next.js/assignment6_2_make_blogs_nxtjs/app/components/index.ts
@@ -1,8 +1,12 @@
 import Hero from "./Hero";
 import CustomButton from "./CustomButton";
+import Footer from "./Footer";
+import Navbar from "./Navbar";
 
 
 export {
     Hero ,
     CustomButton,
+    Navbar,
+    Footer,
 }
--- Assignments_Next.js/assignment6_2_make_blogs_nxtjs/app/page.tsx
+++ Assignments_Next.js/assignment6_2_make_blogs_nxtjs/app/page.tsx
@@ -4,7 +4,7 @@ import { Hero } from './components';
 
 export default function Home() {
   return <main className="overflow-hidden">
-    Hello
+   
   
   <Hero />
   
