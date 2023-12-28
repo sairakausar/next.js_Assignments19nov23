464247eac2729b0ca0dd58702d4ec96893e913b1
sairakausar <sairakausar3@gmail.com>
2023-12-27 20:44:00 -0800

    Car website and now i work on search bar

--- Assignments_Next.js/assignment6_2_make_blogs_nxtjs/app/components/index.ts
+++ Assignments_Next.js/assignment6_2_make_blogs_nxtjs/app/components/index.ts
@@ -2,6 +2,9 @@ import Hero from "./Hero";
 import CustomButton from "./CustomButton";
 import Footer from "./Footer";
 import Navbar from "./Navbar";
+import CustomFilter from "./CustomFilter";
+import SearchBar from "./SearchBar";
+import SearchManufacturer from "./SearchManufacturer";
 
 
 export {
@@ -9,4 +12,7 @@ export {
     CustomButton,
     Navbar,
     Footer,
+    CustomFilter,
+    SearchBar,
+    SearchManufacturer,
 }
