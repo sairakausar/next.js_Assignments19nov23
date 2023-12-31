ab01660feabae3c876adbe190c927c0e569ffb26
sairakausar <sairakausar3@gmail.com>
2023-12-30 18:06:33 -0800

    destructuring and calculation for car rent

--- Assignments_Next.js/assignment6_2_make_blogs_nxtjs/app/components/CarCard.tsx
+++ Assignments_Next.js/assignment6_2_make_blogs_nxtjs/app/components/CarCard.tsx
@@ -6,18 +6,30 @@ import { CarProps } from '../types';
 import { CustomButton } from '.';
 
 
+// Destructuring
+ interface CarPropsType {
+   CallforCarCard: CarProps;
+ }
 
-interface CarPropsType {
-  CallforCarCard: CarProps;
-}
 
 
+const CarCard = ({ CallforCarCard }: CarPropsType) => {
+  const { city_mpg, year, model, make, fuel_type } = CallforCarCard;
 
-const CarCard = (props: any) => {
-    const { city_mpg, class, combination_mpg, cylinders, displacement,
-        drive, fuel_type, highway_mpg, make, model, transmission, year } = CallforCarCard;
-
-  return <div className="">{props.CallforCarCard}</div>;
+  return (
+    <div className="car-card group">
+      <div className="car-card__content">
+        <h2>
+          {year} {make}
+          {model}
+          {city_mpg}
+          {/* {CallforCarCard.city_mpg}
+          {CallforCarCard.class} */}
+          {/* //This is 2nd method of Destructure= data came out from props */}
+        </h2>
+      </div>
+    </div>
+  );
 };
 
 export default CarCard;
