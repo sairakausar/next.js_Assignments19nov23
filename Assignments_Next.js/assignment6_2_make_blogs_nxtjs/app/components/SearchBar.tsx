3ce54bdee3647e9a70a7b994f8212eceeddf6c4c
sairakausar <sairakausar3@gmail.com>
2024-01-01 10:35:45 -0800

    search Button added

--- Assignments_Next.js/assignment6_2_make_blogs_nxtjs/app/components/SearchBar.tsx
+++ Assignments_Next.js/assignment6_2_make_blogs_nxtjs/app/components/SearchBar.tsx
@@ -2,9 +2,12 @@
 
 import { SearchManufacturer } from '@/app/components'
 import { useState } from 'react'
+import {SearchButton} from '@/app/components'
 
 const SearchBar = () => {
-const [manufacturer, SetmMnufacturer] = useState("")
+  const [manufacturer, SetmMnufacturer] = useState("")
+  
+ 
 
     const handleSearch = () => { }
     
@@ -15,6 +18,7 @@ const [manufacturer, SetmMnufacturer] = useState("")
             manufacturer={manufacturer}
             SetmMnufacturer={SetmMnufacturer}
           />
+          <SearchButton otherClasses="sm:hidden" />
         </div>
       </form>
     );
--- Assignments_Next.js/assignment6_2_make_blogs_nxtjs/app/components/index.ts
+++ Assignments_Next.js/assignment6_2_make_blogs_nxtjs/app/components/index.ts
@@ -7,6 +7,7 @@ import SearchBar from "./SearchBar";
 import SearchManufacturer from "./SearchManufacturer";
 import CarCard from "./CarCard";
 import CarDetails from "./CarDetails";
+import SearchButton from "./SearchButton";
 
 
 
@@ -20,4 +21,5 @@ export {
     SearchManufacturer,
     CarCard,
     CarDetails,
+    SearchButton,
 }
