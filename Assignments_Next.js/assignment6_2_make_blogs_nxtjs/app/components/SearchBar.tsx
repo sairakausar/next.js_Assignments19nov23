43f016d4bfd9800995a6c091270676b156e73266
sairakausar <sairakausar3@gmail.com>
2024-01-01 22:25:56 -0800

    carModel search Button

--- Assignments_Next.js/assignment6_2_make_blogs_nxtjs/app/components/SearchBar.tsx
+++ Assignments_Next.js/assignment6_2_make_blogs_nxtjs/app/components/SearchBar.tsx
@@ -2,14 +2,16 @@
 
 import { SearchManufacturer } from '@/app/components'
 import { useState } from 'react'
-import {SearchButton} from '@/app/components'
+import { SearchButton } from '@/app/components'
+import Image from 'next/image'
 
 const SearchBar = () => {
   const [manufacturer, SetmMnufacturer] = useState("")
+  const [carModel , SetCarModel] = useState("")
   
  
 
-    const handleSearch = () => { }
+    const handleSearch = (e: React.FormEvent<HTMLFormElement>) => {e.target.value }
     
     return (
       <form className="searchbar" onSubmit={handleSearch}>
@@ -19,6 +21,26 @@ const SearchBar = () => {
             SetmMnufacturer={SetmMnufacturer}
           />
           <SearchButton otherClasses="sm:hidden" />
+        </div>
+        <div className="searchbar__item">
+          {/* //why icon erase */}
+          <Image
+            src="/model-icon.png"
+            width={25}
+            height={25}
+            className="absolute w-{20px} h-{20px} ml-4"
+            alt="car model"
+          />
+          <input
+            type="text"
+            name="model"
+            value={carModel}
+            onChange={(e) => {
+              SetCarModel(e.target.value);
+            }}
+            placeholder="BMW"
+            className="searchbar__item"
+          />
         </div>
       </form>
     );
