c71b093ef61e680e17135d47cddc2e01df17fc94
sairakausar <sairakausar3@gmail.com>
2024-01-06 07:10:41 -0800

    upload pic on the structure

--- Assignments_Next.js/assignment7_grid_layout/app/page.tsx
+++ Assignments_Next.js/assignment7_grid_layout/app/page.tsx
@@ -5,23 +5,23 @@ export default function Home() {
     <main>
       <div className="grid lg:grid-cols-3 md:grid-cols-3 sm:grid-cols-1 gap-2 m-12  ">
         <div className="relative h-64 border-2 border-black lg:col-span-2 md:col-span-2 sm:col-span-full">
-          A
+          <Image src="/A6.avif" fill alt="imageA" className="" />
         </div>
 
         <div className="relative min-h-64 border-2 border-black lg:row-span-2 md:row-span-2 sm:row-span-1">
-          B
+          <Image src="/rob2.avif" fill alt="imageB" className="" />
         </div>
 
         <div className="relative min-h-64 border-2 border-black lg:row-span-2 md:row-span-2 sm:row-span-1">
-          C
+          <Image src="/sciC.jpg" fill alt="imageB" className="" />
         </div>
 
         <div className="relative h-64 border-2 border-black ">
-          D
+          <Image src="/sci4.webp" fill alt="imageB" className="" />
         </div>
 
         <div className="relative h-64 border-2 border-black lg:col-span-2 md:col-span-2 sm:col-span-full">
-          E
+          <Image src="/A5.jpg" fill alt="imageB" className="" />
         </div>
       </div>
     </main>
