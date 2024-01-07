cbdde83824e8184b43bbb46b08266b0f9706b91d
sairakausar <sairakausar3@gmail.com>
2024-01-07 10:39:01 -0800

    updatee

--- Assignments_Next.js/assignment6_2_make_blogs_nxtjs/app/components/ShowMore.tsx
+++ Assignments_Next.js/assignment6_2_make_blogs_nxtjs/app/components/ShowMore.tsx
@@ -1,9 +1,9 @@
 'use client'
 import React from 'react'
-import { useRouter } from 'next/navigation'
+
 import { ShowMoreProps } from '../types';
 import { CustomButton } from '../components';
-import { updateSearchParams } from '@/utils';
+
 
 
 
