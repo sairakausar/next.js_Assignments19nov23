cea47bb1433d0a9fd4d818d07b87e2b42b3cfca0
sairakausar <sairakausar3@gmail.com>
2024-01-14 09:10:52 -0800

    updated

--- Assignments_Next.js/assignment8_make_website_with_api/app/component/Cards.tsx
+++ Assignments_Next.js/assignment8_make_website_with_api/app/component/Cards.tsx
@@ -7,7 +7,7 @@ const Card = async () => {
     const products = await FetchData();
   return (
     <div className="grid lg:bg-emerald-100 lg:grid-cols-4 md:grid-cols-3 p-7
-     md:bg-orange-300 sm:grid-cols-2 sm:bg-slate-300 xs:grid-cols-1 xxs:mx-0 gap-9 mx-10">
+     md:bg-orange-300 sm:grid-cols-2 sm:bg-slate-300 xs:grid-cols-1 xxs:mx-0 gap-9 mx-10 ">
       {products.map((product: any) => {
         return (
           <div
@@ -15,7 +15,7 @@ const Card = async () => {
             className="border border-blue-700 to-green-400 bg-gradient-to-l
            lg:from-pink-400 lg:to-yellow-300 hover:bg-fuchsia-600 md:from-green-400
            md:to-teal-500 sm:from-amber-300 sm:to-lime-400 xs:from-pink-400
-            xs:to-yellow-300 p-2"
+            xs:to-yellow-300 p-2 rounded-tl-3xl rounded-br-3xl"
           >
             <div className=" relative border-4 lg:border-green-700 border-red-800 h-48">
               <Image src={product.image} fill alt="image" />
