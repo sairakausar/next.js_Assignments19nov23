9fc6fbceeb8e0f3666e7bedb2b95a5f639d4d79d
sairakausar <sairakausar3@gmail.com>
2024-01-14 07:32:35 -0800

    change div colors

--- Assignments_Next.js/assignment8_make_website_with_api/app/component/Cards.tsx
+++ Assignments_Next.js/assignment8_make_website_with_api/app/component/Cards.tsx
@@ -6,20 +6,27 @@ import ShowMoreButton from './ShowMoreButton';
 const Card = async () => {
     const products = await FetchData();
   return (
-    <div className="grid lg:bg-emerald-100 lg:grid-cols-4 md:grid-cols-3 md:bg-teal-300 sm:grid-cols-2 sm:bg-slate-300 xs:grid-cols-1 xxs:mx-0 gap-9 mx-10">
+    <div className="grid lg:bg-emerald-100 lg:grid-cols-4 md:grid-cols-3 p-7
+     md:bg-orange-300 sm:grid-cols-2 sm:bg-slate-300 xs:grid-cols-1 xxs:mx-0 gap-9 mx-10">
       {products.map((product: any) => {
         return (
-          <div key={product.id} className="border border-blue-700 to-green-400  p-2">
-            <div className=" relative border-4 border-red-800 h-48">
+          <div
+            key={product.id}
+            className="border border-blue-700 to-green-400 bg-gradient-to-l
+           lg:from-pink-400 lg:to-yellow-300 md:from-green-400 md:to-teal-500 sm:from-amber-300 sm:to-lime-400 p-2"
+          >
+            <div className=" relative border-4 lg:border-green-700 border-red-800 h-48">
               <Image src={product.image} fill alt="image" />
             </div>
             <div className="border-2 border-green-700 h-12 flex justify-around items-center">
-              <div>Price = {product.price}</div>
+              <div className="relative border-2 border-purple-800 h-8 flex justify-center">
+                Title:
+              </div>
               <div>Category= {product.category}</div>
             </div>
 
             <div className="relative border-2 border-purple-800 h-8 flex justify-center">
-              Title:
+              Price = {product.price}
             </div>
 
             <div>
